c4e948cb869f16601b76e11c1879d9cd0c466f20
Łukasz Tomalczyk <lukasz.tomalczyk@onet.pl>
2017-10-03 19:24:13 +0200

    remove not need lines code from Controller

--- Authentication/Controllers/ValuesController.cs
+++ Authentication/Controllers/ValuesController.cs
@@ -16,39 +16,13 @@ namespace Authentication.Controllers
         {
             this.applicationDbContext = applicationDbContext;
         }
-        // GET api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
         [HttpPost]
         public IActionResult Post([FromBody] User UserFromPost)
         {
             applicationDbContext.Users.Add(UserFromPost);
             applicationDbContext.SaveChanges();
             return Ok(UserFromPost);
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
