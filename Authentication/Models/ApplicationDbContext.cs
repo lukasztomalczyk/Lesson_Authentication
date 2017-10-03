8cf9ccfb63afbfd8665634ac3674e103fa2531b1
Łukasz Tomalczyk <lukasz.tomalczyk@onet.pl>
2017-10-03 19:22:19 +0200

    change MySQL to MySql - Pamelo.EntityFramework

--- Authentication/Models/ApplicationDbContext.cs
+++ Authentication/Models/ApplicationDbContext.cs
@@ -1,17 +1,25 @@
+using System;
 using Authentication.DbModel;
 using Microsoft.EntityFrameworkCore;
+
 
 namespace Authentication.Models
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
+        public ApplicationDbContext()
         {
         }
 
         public DbSet<Employee> Employees { get; set; }
         public DbSet<User> Users { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+          optionsBuilder.UseMySql("Host=localhost;Port=3306;database=LessonAuthentication;user=root;password=maksimus666" );
+
+
+        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<User>()
@@ -19,9 +27,6 @@ namespace Authentication.Models
                         .HasKey(key => key.UserName);
         }
 
-/*         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-          optionsBuilder.UseMySQL("server=localhost;database=LessonAuthentication;user=root;password=maksimus666");
-        } */
+
     }
 }
